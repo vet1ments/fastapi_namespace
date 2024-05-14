0bef26216b1352bdfd1c46535fc64f23910d701b
nohongseok <vet1ments@naver.com>
2024-05-15 00:59:52 +0900

    readme.me

--- setup.py
+++ setup.py
@@ -5,13 +5,15 @@ from setuptools import find_packages, setup
 setup(
     name='fastapi_namespace',
     description="For FastAPI Routing Class",
-    version='0.1.3',
+    version='0.1.4',
     python_requires='>=3.11.0',
     author="no hong seok",
     author_email="vet1ments@naver.com",
     maintainer="no hong seok",
     maintainer_email="vet1ments@naver.com",
-    download_url="https://github.com/vet1ments/fastapi_namespace",
+    project_urls={
+        "Repository": "https://github.com/vet1ments/fastapi_namespace"
+    },
     license="MIT",
     packages=find_packages(),
     install_requires=[
