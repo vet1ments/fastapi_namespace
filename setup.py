b6a1e7ddab0d6ec6ddda294e00daa2a46034f8bc
nohongseok <vet1ments@naver.com>
2024-05-15 00:29:13 +0900

    240515

--- setup.py
+++ setup.py
@@ -4,18 +4,15 @@ from setuptools import find_packages, setup
 
 setup(
     name='fastapi_namespace',
-    version='0.1.0',
+    version='0.1.1',
     python_requires='>=3.11.0',
     author="no hong seok",
     author_email="vet1ments@naver.com",
-    packages=find_packages(where='fastapi_namespace'),
-    package_dir={'': 'fastapi_namespace'},
+    packages=find_packages(),
     install_requires=[
         "fastapi>=0.108.0"
     ],
     tests_require=[
         "uvicorn>=0.9.0"
     ],
-    py_modules=[splitext(basename(path))[0] for path in glob('fastapi_namespace/*.py')],
-    download_url=r"https://github.com/vet1ments/fastapi_namespace"
 )
