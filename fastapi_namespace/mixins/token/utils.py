586775dc51dd13a4fb8c39925c5252610dad976a
nohongseok <vet1ments@naver.com>
2024-05-28 06:19:06 +0900

    mixins\token\utils validate 에 configdict forbid 추가 240528

--- fastapi_namespace/mixins/token/utils.py
+++ fastapi_namespace/mixins/token/utils.py
@@ -1,26 +1,48 @@
-from pydantic import TypeAdapter, ValidationError
-from .typings import OpaqueToken, OpaqueTokenInfo, JWTToken, JWTTokenInfo
+from pydantic import TypeAdapter, ValidationError, ConfigDict
+from .typings import (
+    OpaqueToken as OpaqueToken_,
+    OpaqueTokenInfo as OpaqueTokenInfo_,
+    JWTToken as JWTToken_,
+    JWTTokenInfo as JWTTokenInfo_,
+)
+from typing import Literal
 
-OpaqueTokenValidator = TypeAdapter(OpaqueToken)
-OpaqueTokenInfoValidator = TypeAdapter(OpaqueTokenInfo)
-JWTTokenValidator = TypeAdapter(JWTToken)
-JWTTokenInfoValidator = TypeAdapter(JWTTokenInfo)
+class OpaqueToken(OpaqueToken_):
+    pass
+class OpaqueTokenInfo(OpaqueTokenInfo_):
+    pass
+class JWTToken(JWTToken_):
+    pass
+class JWTTokenInfo(JWTTokenInfo_):
+    pass
 
 def _default_validate(validator: TypeAdapter, data: dict) -> bool:
     try:
-        validator.validate_python(data)
+        validator.validate_python(data, strict=True)
         return True
     except ValidationError as e:
         return False
 
-def validate_opaque_token(data: dict) -> bool:
+def validate_opaque_token(data: dict, extra: Literal['forbid', 'allow', 'ignore'] = 'forbid') -> bool:
+    OpaqueToken.__pydantic_config__ = ConfigDict(extra=extra)
+    OpaqueTokenValidator = TypeAdapter(OpaqueToken)
+    OpaqueToken.__pydantic_config__ = None
     return _default_validate(OpaqueTokenValidator, data)
 
-def validate_opaque_token_info(data: dict) -> bool:
+def validate_opaque_token_info(data: dict, extra: Literal['forbid', 'allow', 'ignore'] = 'forbid') -> bool:
+    OpaqueTokenInfo.__pydantic_config__ = ConfigDict(extra=extra)
+    OpaqueTokenInfoValidator = TypeAdapter(OpaqueTokenInfo)
+    OpaqueTokenInfo.__pydantic_config__ = None
     return _default_validate(OpaqueTokenInfoValidator, data)
 
-def validate_jwt_token(data: dict) -> bool:
+def validate_jwt_token(data: dict, extra: Literal['forbid', 'allow', 'ignore'] = 'forbid') -> bool:
+    JWTToken.__pydantic_config__ = ConfigDict(extra=extra)
+    JWTTokenValidator = TypeAdapter(JWTToken)
+    JWTToken.__pydantic_config__ = None
     return _default_validate(JWTTokenValidator, data)
 
-def validate_jwt_token_info(data: dict) -> bool:
+def validate_jwt_token_info(data: dict, extra: Literal['forbid', 'allow', 'ignore'] = 'forbid') -> bool:
+    JWTTokenInfo.__pydantic_config__ = ConfigDict(extra=extra)
+    JWTTokenInfoValidator = TypeAdapter(JWTTokenInfo)
+    JWTTokenInfo.__pydantic_config__ = None
     return _default_validate(JWTTokenInfoValidator, data)
